fedcc0c5f14a648995242f1773403733f5bfd8b2
SULE BALABAN <sule.balaban27@gmail.com>
2025-03-03 14:43:00 +0100

    Fix:deployment issue

--- smartedu/settings.py
+++ smartedu/settings.py
@@ -126,11 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'smartedu/static'), ]
+# .\env\Scripts\activate
 
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Media files
 
